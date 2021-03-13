59e84b9a70d4f42239d4863ad5b9352b76f67044
Hayden <Haydenleete@gmail.com>
2021-03-14 02:18:52 +1300

    Commented ready function

--- Scenes/Launcher/Launcher.gd
+++ Scenes/Launcher/Launcher.gd
@@ -3,7 +3,7 @@ extends Control
 var save_location = OS.get_user_data_dir()+"/SavedGames/"
 var current_version = "v0.2"
 var updating = false
-var update_file_size := 0
+var update_file_size := 0.0
 
 onready var req = $HTTPRequest
 onready var games_list = $MarginContainer/VBoxContainer/Control/VBoxContainer/MarginContainer2/ScrollContainer/GamesList
@@ -14,40 +14,52 @@ onready var download_label = $MarginContainer/VBoxContainer/DownloadLabel
 onready var progress_bar = $MarginContainer/VBoxContainer/ProgressBar
 
 func _ready():
-	
 	var dirManager = Directory.new()
+	if not dirManager.dir_exists(save_location):
+		dirManager.make_dir(save_location)
+	
 	title_label.text += " " + current_version
+
+	# Handle extra arguments
 	for arg in OS.get_cmdline_args():
+		# If the launcher has been opened as the result of an update. 
+		# Proceed to delete and replace the old launcher.
 		if arg == "updated":
 			var exec_path = OS.get_executable_path()
 			var exec_fname = exec_path.get_file()
 			dirManager.open(exec_path.get_base_dir())
-			dirManager.remove(exec_path.get_file().split(".tmp")[0] + ".exe")
-			dirManager.rename(exec_path.get_file(), exec_path.get_file().split(".tmp")[0] + ".exe")
+			dirManager.remove(exec_fname.split(".tmp")[0] + ".exe")
+			dirManager.rename(exec_fname, exec_path.get_file().split(".tmp")[0] + ".exe")
 		
+		# If any of the arguments are paths, it will be from the user dragging
+		# a file onto the exe. Attempt to import these as games.
 		elif arg.replace("\\", "/").is_abs_path():
 			import_game(arg.replace("\\", "/"))
-		
-	if not dirManager.dir_exists(save_location):
-		dirManager.make_dir_recursive(save_location)
 	
-	# warning-ignore:return_value_discarded
-	get_tree().connect("files_dropped", self, "import_games_from_drop")
+	self.get_tree().connect("files_dropped", self, "import_games_from_drop")
 	refresh_games_list()
 	
+	# Check for new launcher version
 	req.connect("request_completed", self, "version_request_completed")
 	req.request("https://github.com/Haydoggo/GodotGameLauncher/releases/latest")
-	
 
-func version_request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray):
+func _process(_delta):
+	if updating:
+		download_label.text = "%dkB of %dkB downloaded" % [req.get_downloaded_bytes()/1024, update_file_size/1024]
+		progress_bar.value = req.get_downloaded_bytes()/float(update_file_size) * 100.0
+
+func version_request_completed(_result: int, response_code: int, _headers: PoolStringArray, body: PoolByteArray):
 	req.disconnect("request_completed", self, "version_request_completed")
+	
+	# Search body for latest version number
 	if response_code == HTTPClient.RESPONSE_OK:
 		var s := body.get_string_from_utf8()
 		var tag_token = "tag/"
 		var version_num_start = s.find(tag_token) + tag_token.length()
 		var version_num_end = s.find("\"", version_num_start)
 		var latest_version = s.substr(version_num_start, version_num_end - version_num_start)
-		if latest_version != current_version:
+		
+		if current_version != latest_version:
 			update_launcher_button.visible = true
 			update_launcher_button.connect("pressed", self, "update_launcher", [latest_version])
 
@@ -61,7 +73,7 @@ func update_launcher(version : String):
 	
 	# Wait until file size is fetched
 	yield(req, "request_completed")
-	# Download update to Launcher.tmp
+	# Download update as .tmp
 	req.download_file = exec_path.split(".exe")[0] + ".tmp"
 	req.request(download_url)
 	progress_bar.visible = true
@@ -71,10 +83,10 @@ func update_launcher(version : String):
 	
 	# Wait until file size is downloaded
 	yield(req, "request_completed")
-	OS.execute(req.download_file, ["updated"], false)
+	var _pid = OS.execute(req.download_file, ["updated"], false)
 	get_tree().quit()
 	
-func update_request_complete(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray):
+func update_request_complete(_result: int, _response_code: int, headers: PoolStringArray, _body: PoolByteArray):
 	req.disconnect("request_completed", self, "update_request_complete")
 	for header in headers:
 		if header.begins_with("Content-Length"):
@@ -99,11 +111,6 @@ func refresh_games_list():
 			file_name = dir.get_next()
 	emptyLabel.visible = (games_list.get_child_count() == 0)
 
-func _process(delta):
-	if updating:
-		download_label.text = "%dkB of %dkB downloaded" % [req.get_downloaded_bytes()/1024, update_file_size/1024]
-		progress_bar.value = req.get_downloaded_bytes()/float(update_file_size) * 100.0
-
 func _on_ImportGame_pressed():
 	var fileDiag = FileDialog.new()
 	add_child(fileDiag)
@@ -114,7 +121,7 @@ func _on_ImportGame_pressed():
 	fileDiag.access = FileDialog.ACCESS_FILESYSTEM
 	fileDiag.popup_centered_ratio()
 	fileDiag.connect("file_selected", self, "import_game")
-
+	fileDiag.connect("files_selected", self, "import_games")
 
 func _on_LoadGame_pressed():
 	var fileDiag = FileDialog.new()
@@ -128,7 +135,6 @@ func _on_LoadGame_pressed():
 	fileDiag.connect("files_selected", self, "open_games")
 	fileDiag.current_dir = save_location
 	fileDiag.popup_centered_ratio()
-
 
 func import_game(filePath:String):
 	print("importing game")
@@ -145,11 +151,9 @@ func import_game(filePath:String):
 	else:
 		save_game(filePath)
 
-
 func import_games(filePaths:PoolStringArray):
 	for filePath in filePaths:
 		import_game(filePath.replace("\\", "/"))
-
 
 func save_game(filePath:String):
 	var fileName = filePath.substr(filePath.find_last("/")+1, -1)
@@ -160,7 +164,6 @@ func save_game(filePath:String):
 	Directory.new().copy(filePath, save_location + fileName)
 	refresh_games_list()
 
-
 func open_game(filePath:String):
 	print(filePath)
 	if filePath.get_extension() != "pck":
@@ -170,17 +173,14 @@ func open_game(filePath:String):
 		var fileName = filePath.substr(filePath.find_last("/")+1, -1)
 		print("path: " + path)
 		print("fileName: " + fileName)
-		# warning-ignore:return_value_discarded
-		OS.execute(OS.get_executable_path(),["--main-pack", fileName, "--path", path],false)
-
+		var _pid = OS.execute(OS.get_executable_path(),["--main-pack", fileName, "--path", path],false)
 
 func open_games(filePaths:PoolStringArray):
 	for filePath in filePaths:
 		open_game(filePath.replace("\\", "/"))
 
-
 func import_games_from_drop(filePaths:PoolStringArray, _fromMonitor:int):
 	import_games(filePaths)
 	
 func open_games_folder():
-	OS.shell_open(save_location)
+	var _err = OS.shell_open(save_location)
